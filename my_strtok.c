6c4a9b853f133829689fb80eed75c43a71134000
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-04-27 19:10:02 +0300

    ALX

--- my_strtok.c
+++ my_strtok.c
@@ -1,35 +1,45 @@
 #include "shell.h"
+/**
+ * _strtok - Tokenizes a string into substrings based on a delimiter.
+ * @str: a character pointer representing the string to be tokenized.
+ * @delim: represents the delimiter used to tokenize the string.
+ * return: pointer
+ */
+_
+char *_strtok(char *str, const char *delim)
+{
+	static char *ptr;
+	char *start;
 
+	if (str == NULL)
+	{
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		str = ptr;
+	}
+	start = str;
+	while (*str != '\0')
+	{
+		const char *d = delim;
 
-char* _strtok(char* str, const char* delim) {
-    static char* ptr = NULL;
-    char* start;
-  
-    if (str == NULL) {
-        if (ptr == NULL) {
-            return NULL;
-        }
-        str = ptr;
-    }
-    
-    start = str;
-    while (*str != '\0') {
-        const char* d = delim;
-        while (*d != '\0') {
-            if (*str == *d) {
-                *str = '\0';
-                ptr = str + 1;
-                if (*ptr == '\0') {
-                    ptr = NULL;
-                }
-                return start;
-            }
-            d++;
-        }
-        str++;
-    }
-    
-    ptr = NULL;
-    return start;
+		while (*d != '\0')
+		{
+			if (*str == *d)
+			{
+				*str = '\0';
+				ptr = str + 1;
+				if (*ptr == '\0')
+				{
+					ptr = NULL;
+				}
+				return (start);
+			}
+			d++;
+		}
+		str++;
+	}
+	ptr = NULL;
+	return (start);
 }
-
