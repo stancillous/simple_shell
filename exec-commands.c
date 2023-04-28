0648b2b160008d58463f6cb392fbb0f19e08d315
stancillous <stancillousray@gmail.com>
2023-04-28 02:48:15 -0700

    changes

--- exec-commands.c
+++ exec-commands.c
@@ -9,31 +9,13 @@
  **/
 void handle_command_with_args(const char *command, char *args[])
 {
-	pid_t pid = fork(); /* Fork a child process*/
+	pid_t pid = 0; /* Fork a child process*/
 
 	if (pid == 0)
 	{/* Child process*/
 		execvp(command, args); /* Execute the command with arguments*/
 		perror("execvp"); /* Print error message if execvp fails*/
 		exit(EXIT_FAILURE); /* Exit child process with failure status*/
-	}
-	else if (pid > 0)
-	{/* Parent process*/
-		int status;
-
-		waitpid(pid, &status, 0); /* Wait for the child process to complete*/
-		/*
-		 * if (WIFEXITED(status))
-		{
-			int exit_status = WEXITSTATUS(status);
-			printf("Child process exited with status: %d\n", exit_status);
-		}
-		else if (WIFSIGNALED(status))
-		{
-			int signal_number = WTERMSIG(status);
-			printf("Child process terminated with signal: %d\n", signal_number);
-		}
-		*/
 	}
 	else
 	{
